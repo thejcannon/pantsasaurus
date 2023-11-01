5bc901fd512109023949c7c7ac6cc20e80c7e50d
Joshua <joshdcannon@gmail.com>
2023-11-01 11:27:37 -0500

    fix index

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Layout from '@theme/Layout';
+import MDXContent from '@theme/MDXContent';
 import Index from './_index.mdx';
 
 export default function Home(): JSX.Element {
   return (
-    <Layout
-      description="Pantsbuild: The ergonomic build system">
+    <MDXContent>
       <Index />
-    </Layout>
+    </MDXContent>
   );
 }
