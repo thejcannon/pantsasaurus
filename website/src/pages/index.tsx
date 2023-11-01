0a47e135b57d1b09be1ed4fdf2338734cf49fd54
Joshua <joshdcannon@gmail.com>
2023-11-01 11:32:45 -0500

    layout

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import MDXContent from '@theme/MDXContent';
+import Layout from '@theme/Layout';
 import Index from './_index.mdx';
 
 export default function Home(): JSX.Element {
   return (
-    <MDXContent>
-      <Index />
-    </MDXContent>
+    <Layout>
+      <MDXContent>
+        <Index />
+      </MDXContent>
+    </Layout>
   );
 }
